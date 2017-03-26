673d3645ae1304671942096956fee40d1ddcbe65
Russ Katz <russkatz@rkatz-rmbp15.local>
2017-03-26 15:54:12 -0500

    adding scala for results

--- stream-framework/consumer/src/main/scala/streamingDemo.scala
+++ stream-framework/consumer/src/main/scala/streamingDemo.scala
@@ -1,31 +1,13 @@
 package com.datastax.demo
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * Created by carybourgeois on 10/30/15.
-  *  Modified by jasonhaugland on 10/20/16.
- */
 
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.writer.{TTLOption, WriteConf}
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
+
+import scala.collection.JavaConversions._
 import kafka.serializer.StringDecoder
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SQLContext, SaveMode}
@@ -33,16 +15,35 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext, Time}
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class UserStats(user: String, ts: String, arg1: Int, arg2: Int, arg3: Int)
+
 object SparkKafkaConsumer extends App {
 
   val appName = "test"
   val topicName = "test"
+  val cassandra_host = "10.255.0.120"
 
   val conf = new SparkConf().setAppName(appName)
   val sc = SparkContext.getOrCreate(conf)
 
   val sqlContext = SQLContext.getOrCreate(sc)
   import sqlContext.implicits._
+
+
+    val cluster = Cluster.builder().addContactPoint(cassandra_host).build()
+    val session = cluster.connect()
+    session.execute("DROP KEYSPACE IF EXISTS demo")
+    session.execute("CREATE KEYSPACE demo WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 }")
+    session.execute(
+      """CREATE TABLE demo.user_stats (
+        |user TEXT,
+        |ts TEXT,
+        |arg1 INT,
+        |arg2 INT,
+        |arg3 INT,
+        |PRIMARY KEY (user,ts)
+        )""".stripMargin)
+    session.close()
 
   val ssc = new StreamingContext(sc, Milliseconds(10000))
   ssc.checkpoint(appName)
@@ -53,7 +54,29 @@ object SparkKafkaConsumer extends App {
 
   val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, kafkaTopics)
 
-  kafkaStream.print
+  // kafkaStream.print
+  kafkaStream
+    .foreachRDD {
+      (message: RDD[(String, String)], batchTime: Time) => {
+        val df = message.map {
+          case (k, v) => v.split(",")
+        }.map(payload => {
+          val ts = System.currentTimeMillis().toString
+          UserStats(payload(0), ts,  payload(1).toInt, payload(2).toInt, payload(3).toInt)
+        }).toDF("user", "ts", "arg1", "arg2","arg3")
+        df
+          .write
+          .format("org.apache.spark.sql.cassandra")
+          .mode(SaveMode.Append)
+          .options(Map("keyspace" -> "demo", "table" -> "user_stats"))
+          .save()
+      }
+    }
+
+  //val json = sc.parallelize(Seq(kafkaStream)) //.saveToCassandra("demo", "user_stats")
+  //json.foreach { x => print(x) }
+
+  //sqlContext.jsonRDD(json).map(MonthlyCommits(_)).saveToCassandra("githubstats","monthly_commits")
 
   ssc.start()
   ssc.awaitTermination()
