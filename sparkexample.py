3b4929e0ea0679d57049b6e97c89dd975c48070f
Russ Katz <russkatz@gmail.com>
2019-03-27 09:44:12 -0500

    Update sparkexample.py

--- sparkexample.py
+++ sparkexample.py
@@ -6,7 +6,7 @@ conf = SparkConf().setAppName("Stand Alone Python Script")
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-#load data from DSE
+#load table from DSE
 table = sqlContext.read.format("org.apache.spark.sql.cassandra").load(table="table1", keyspace="demo")
 
 #Filter data
@@ -15,3 +15,9 @@ rows.show()
 
 #Write data to DSEFS
 rows.write.mode('append').parquet("demo2/table1.pqt/")
+
+#Load data from DSEFS
+parquetFile = sqlContext.read.parquet("demo2/table1.pqt/")
+parquetFile.createOrReplaceTempView("parquetFile")
+rows = sqlContext.sql("SELECT * FROM parquetFile where data2 = 25")
+rows.show()
