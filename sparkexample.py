9207589be3571b7d1142123107197001ad289fc1
Russ Katz <russkatz@gmail.com>
2019-03-27 09:21:45 -0500

    Create sparkexample.py

---
+++ sparkexample.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
+
+#Setup Spark Context for DSE
+conf = SparkConf().setAppName("Stand Alone Python Script")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+
+#load data from DSE
+table = sqlContext.read.format("org.apache.spark.sql.cassandra").load(table="table1", keyspace="demo")
+
+#Filter data
+rows = table.where(table.bucket==20193261840).where(table.data1==61)
+rows.show()
+
+#Write data to DSEFS
+rows.write.mode('append').parquet("demo2/table1.pqt/")
